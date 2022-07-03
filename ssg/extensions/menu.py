ff7ab80cca07cdbd84c16ea30f173f942fc5be41
Tom Whitty <tomwhitty7@gmail.com>
2022-07-02 19:59:03 -0500

    Update menu.py

--- ssg/extensions/menu.py
+++ ssg/extensions/menu.py
@@ -16,6 +16,6 @@ def collect_files(source, site_parsers):
 @hooks.register("generate_menu")
 def generate_menu(html, ext):
     template = '<li><a href="{}{}">{}</a></li>'
-    menu_item = lambda name, ext: template.format(name, ext,name.title())
+    menu_item = lambda name, ext: template.format(name, ext, name.title())
     menu = "\n".join([menu_item(path.stem, ext) for path in files])
-    return "<ul>\n{}<ul>\n{}".format(menu, html)
+    return "<ul>\n{}</ul>\n{}".format(menu, html)
